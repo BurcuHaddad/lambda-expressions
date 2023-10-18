4f224836a9442e8c36b8fd5f22dec80d892fc2ee
BurcuHaddad <burcuhaddad@gmail.com>
2023-10-18 21:14:30 +0300

    functional interfaces

--- src/dev/bh/Main.java
+++ src/dev/bh/Main.java
@@ -3,8 +3,10 @@ package dev.bh;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
@@ -54,6 +56,31 @@ public class Main {
         list.forEach(s -> System.out.println(s));
 
         list.replaceAll(s -> s.charAt(0) + "-" + s.toUpperCase());
+        System.out.println("------");
+        list.forEach(s -> System.out.println(s));
+
+        String[] emptyStrings = new String[10];
+        System.out.println(Arrays.toString(emptyStrings));
+        Arrays.fill(emptyStrings, "");
+        System.out.println(Arrays.toString(emptyStrings));
+
+        Arrays.setAll(emptyStrings, (i) -> "" + (i + 1) + ". ");
+        System.out.println(Arrays.toString(emptyStrings));
+
+        Arrays.setAll(emptyStrings, (i) -> "" + (i + 1) + ". "
+            + switch (i) {
+                    case 0 -> "one";
+                    case 1 -> "two";
+                    case 2 -> "three";
+                default -> "";
+                }
+        );
+        System.out.println(Arrays.toString(emptyStrings));
+
+        String[] names = {"Ann", "Bob", "Carol", "David", "Ed", "Fred"};
+        String[] randomList = randomlySelectedValues(15, names,
+                () -> new Random().nextInt(0, names.length));
+        System.out.println(Arrays.toString(randomList));
 
     }
     public static <T> T calculator(BinaryOperator<T> function, T value1, T value2) {
@@ -63,5 +90,12 @@ public class Main {
     }
     public static <T> void processPoint(T t1, T t2, BiConsumer<T,T> consumer) {
         consumer.accept(t1, t2);
+    }
+    public static String[] randomlySelectedValues(int count, String[] values, Supplier<Integer> s) {
+        String[] selectedValue = new String[count];
+        for (int i = 0; i < count; i++) {
+            selectedValue[i] = values[s.get()];
+        }
+        return selectedValue;
     }
 }
